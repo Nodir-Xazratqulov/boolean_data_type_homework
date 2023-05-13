f9a9b0e636c6ed65c7c43b0e3f60888fffe93852
Nodir-Xazratqulov <nodirxazratqulov2110@gmail.com>
2023-05-13 14:59:57 +0500

    bool10

--- bool10.py
+++ bool10.py
@@ -1,3 +1,4 @@
+import math
 def main(a):
     """
     Check that the number "a" is a perfect square.
@@ -7,4 +8,5 @@ def main(a):
         bool
     """
     # Write your code here
-    return 
+    return int(a**0.5)**2==a
+print(main(225))
